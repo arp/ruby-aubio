046486f8c1dea94dc83427e9856ca39d8f88eb81
Arthur Pirogovski <arthur@flyingtealeaf.com>
2017-10-23 00:25:36 -0400

    patches for version-less LLVM and aubio 0.4.6

--- aubio-ffi-generator.rb
+++ aubio-ffi-generator.rb
@@ -4,9 +4,9 @@ require 'ffi_gen'
 
 FFIGen.generate(
   module_name: "Aubio::Api",
-  ffi_lib:     "/usr/local/Cellar/aubio/0.4.4/lib/libaubio.dylib",
-  headers:     Dir["/usr/local/Cellar/aubio/0.4.4/include/aubio/**/*.h"],
-  cflags:      `/usr/local/opt/llvm35/bin/llvm-config-3.5 --cflags`.split(" "),
+  ffi_lib:     "/usr/local/Cellar/aubio/0.4.6/lib/libaubio.dylib",
+  headers:     Dir["/usr/local/Cellar/aubio/0.4.6/include/aubio/**/*.h"],
+  cflags:      `/usr/local/opt/llvm/bin/llvm-config --cflags`.split(" "),
   # the following can be used to trim the aubio_ from the generated function names
   # prefixes:    ["aubio_", "CX"],
   prefixes:    [],
